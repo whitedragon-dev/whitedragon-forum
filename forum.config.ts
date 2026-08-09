c8580c18e3ca4a66c1007b008446c977458ec4d5
whitedragon-one <300095696+whitedragon-one@users.noreply.github.com>
2026-08-09 12:24:42 +0800

    Update forum.config.ts

--- forum.config.ts
+++ forum.config.ts
@@ -31,6 +31,7 @@ export default defineForumConfig({
 		oauth: {
 			clientId: 'Ov23ctmoDN6GtAyNvRcE',
 			proxyUrl: 'https://whitedragon-forum-oauth.whitedragon-dev.workers.dev'
+			scope: 'read:org'
 		}
 	},
 
